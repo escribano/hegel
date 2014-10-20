047bbc71536b3ba44d028ddc232a2a2091eb7ff2
Ademir Escribano <ademir.escribano@gmail.com>
2014-10-20 18:09:37 -0200

    auto

--- lib/xinstall.sh
+++ lib/xinstall.sh
@@ -54,7 +54,7 @@ function hegel.setup.plato () {
   #INSTALL_DIR="/opt/hab/root"
   sudo rm -rf $install_dir && sudo mkdir -p $install_dir
   #sudo cp -R $HEGEL_PATH/site $INSTALL_DIR/
-  tar xzvf /tmp/$package_name.tar.gz -C $install_dir
+  sudo tar xzvf /tmp/$package_name.tar.gz -C $install_dir
   #pushd $PHILOSOPHIE_XPATH/.. > /dev/null
   #git clone git@github.com:escribano/$package_name.git
   #cd kant
