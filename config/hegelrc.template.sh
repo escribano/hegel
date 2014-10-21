28316e744ff7e06f85a0d0208648be7a1de82967
Ademir Escribano <ademir.escribano@gmail.com>
2014-10-21 13:26:18 -0200

    auto

--- config/hegelrc.template.sh
+++ config/hegelrc.template.sh
@@ -4,10 +4,10 @@ export PHILOSOPHIE_XPATH="$PHILOSOPHIE_PATH"
 # plato
 export ROOTSITE_PATH="/opt/hab/root"
 # socrates
-export DAEMON_PATH="/opt/hab/daemon/sein"
-export DAEMON_CONFIG_PATH="/etc/sein/"
+export DAEMON_PATH="/opt/hab/daemon/socrates"
+export DAEMON_CONFIG_PATH="/etc/philosophie/socrates"
 # heidegger
 export MAPA_PATH="/opt/hab/ng/city/saopaulo"
 
-
+export DATA_PATH="/opt/hab/data"
 
