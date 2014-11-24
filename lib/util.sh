c4475656eee2c9580ae2ebf8f01275be8ea128b9
Ademir Escribano <ademir.escribano@gmail.com>
2014-11-24 06:22:19 -0200

    auto

--- lib/util.sh
+++ lib/util.sh
@@ -9,7 +9,7 @@ function update.config () {
   fi
 }
 
-function hegel.update () {
+function update.hegel () {
   if [ "$UNAME" == "Darwin" ]; then
     _fatal.error
   elif [ "$UNAME" == "Linux" ]; then
