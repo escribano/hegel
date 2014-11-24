6728dcbf1f2608c10794c03ee1b9d548f6a3f6df
Ademir Escribano <ademir.escribano@gmail.com>
2014-11-24 06:30:34 -0200

    auto

--- lib/aws/postgres.sh
+++ lib/aws/postgres.sh
@@ -1,37 +1,37 @@
-function set.locale.en () {
-  LANG=pt_BR.UTF-8
-  LANGUAGE=
-  LC_CTYPE="en_US.UTF-8"
-  LC_NUMERIC="en_US.UTF-8"
-  LC_TIME="en_US.UTF-8"
-  LC_COLLATE="en_US.UTF-8"
-  LC_MONETARY="en_US.UTF-8"
-  LC_MESSAGES="en_US.UTF-8"
-  LC_PAPER="en_US.UTF-8"
-  LC_NAME="en_US.UTF-8"
-  LC_ADDRESS="en_US.UTF-8"
-  LC_TELEPHONE="en_US.UTF-8"
-  LC_MEASUREMENT="en_US.UTF-8"
-  LC_IDENTIFICATION="en_US.UTF-8"
-  LC_ALL=en_US.UTF-8
+function set.locale.br () {
+  export LANG=pt_BR.UTF-8
+  # export LANGUAGE=
+  # export LC_CTYPE="en_US.UTF-8"
+  # export LC_NUMERIC="en_US.UTF-8"
+  # export LC_TIME="en_US.UTF-8"
+  # export LC_COLLATE="en_US.UTF-8"
+  # export LC_MONETARY="en_US.UTF-8"
+  # export LC_MESSAGES="en_US.UTF-8"
+  # export LC_PAPER="en_US.UTF-8"
+  # export LC_NAME="en_US.UTF-8"
+  # export LC_ADDRESS="en_US.UTF-8"
+  # export LC_TELEPHONE="en_US.UTF-8"
+  # export LC_MEASUREMENT="en_US.UTF-8"
+  # export LC_IDENTIFICATION="en_US.UTF-8"
+  # export LC_ALL=en_US.UTF-8
 }
 
-function set.locale.br () {
-  LANG=en_US.UTF-8
-  LANGUAGE=
-  LC_CTYPE="en_US.UTF-8"
-  LC_NUMERIC="en_US.UTF-8"
-  LC_TIME="en_US.UTF-8"
-  LC_COLLATE="en_US.UTF-8"
-  LC_MONETARY="en_US.UTF-8"
-  LC_MESSAGES="en_US.UTF-8"
-  LC_PAPER="en_US.UTF-8"
-  LC_NAME="en_US.UTF-8"
-  LC_ADDRESS="en_US.UTF-8"
-  LC_TELEPHONE="en_US.UTF-8"
-  LC_MEASUREMENT="en_US.UTF-8"
-  LC_IDENTIFICATION="en_US.UTF-8"
-  LC_ALL=en_US.UTF-8
+function set.locale.us () {
+  export LANG=en_US.UTF-8
+  # export LANGUAGE=
+  # export LC_CTYPE="en_US.UTF-8"
+  # export LC_NUMERIC="en_US.UTF-8"
+  # export LC_TIME="en_US.UTF-8"
+  # export LC_COLLATE="en_US.UTF-8"
+  # export LC_MONETARY="en_US.UTF-8"
+  # export LC_MESSAGES="en_US.UTF-8"
+  # export LC_PAPER="en_US.UTF-8"
+  # export LC_NAME="en_US.UTF-8"
+  # export LC_ADDRESS="en_US.UTF-8"
+  # export LC_TELEPHONE="en_US.UTF-8"
+  # export LC_MEASUREMENT="en_US.UTF-8"
+  # export LC_IDENTIFICATION="en_US.UTF-8"
+  # export LC_ALL=en_US.UTF-8
 }
 
 
@@ -41,6 +41,13 @@ function install.postgres () {
   set.locale.en
 }
 
+function recreate.cluster () {
+  set.locale.br
+  pg_dropcluster --stop 9.4 main
+  #pg_createcluster --locale de_DE.UTF-8 --start 8.3 main
+  pg_createcluster --locale --start 9.4 main
+  set.locale.en
+}
 
 
 function more.locales () {
